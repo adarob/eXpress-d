318c7dc6f43618bfa24462b6f0f59a925031123a
Allen Day <allenday@users.noreply.github.com>
2015-11-30 18:04:36 +0800

    comment cleanup

--- src/main/scala/expressd/ExpressD.scala
+++ src/main/scala/expressd/ExpressD.scala
@@ -881,12 +881,6 @@ object ExpressD {
 
 
           for (i <- 0 until numAlignments) {
-            //if ( left._1(i) > right._1(i) ) {
-            //  val t = left
-            //  left = right
-            //  right = t
-            //}
-
             val pairTargetId = fragment._1(i)
             val leftFirst = fragment._2(i)
             val leftStart = left._1(i)
@@ -900,10 +894,8 @@ object ExpressD {
             val misc2R = right._4(i)
             val misc3R = right._5(i)
 
-            //val pairLength = rightStart - leftStart + 1
             val pairLength = Math.abs(rightStart - leftStart) + 1
 
-            //println("pairLength=" + pairLength + " pairTargetId=" + pairTargetId)
             val bcTausValue = bcTaus.value(pairTargetId)
             val bcFldValue = bcFld.value(pairLength)
             val bcEffLengthsValue = bcEffLengths.value(pairTargetId)
@@ -1039,7 +1031,6 @@ object ExpressD {
             val misc2R = right._4(i)
             val misc3R = right._5(i)
 
-            //val pairLength = rightStart - leftStart + 1
             val pairLength = Math.abs(rightStart - leftStart) + 1
 
             // Correct for numerical issues
